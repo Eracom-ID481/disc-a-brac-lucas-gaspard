1540e43aa6cf2354298a013b6e7e21bffef67d3c
Balzami <lucas.tripod@gmail.com>
2020-11-18 11:50:50 +0100

    18.11.20a

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 $(function() {
 
-    $(".box-hidden").slice(0, 3).show();
+    $(".box-hidden").slice(0, 4).show();
 
     $("#loadMore").on('click', function(e) {
         console.log('wtf?');
         e.preventDefault();
-        $(".box-hidden:hidden").slice(0, 3).slideDown();
+        $(".box-hidden:hidden").slice(0, 4).slideDown();
         if ($(".box-hidden:hidden").lenght == 0) {
             $("#load").fadeOut('slow');
         }
         $('html,body').animate({
             scrollTop: $(this).offset().top
-        }, 1500);
+        }, 500);
     });
 });
